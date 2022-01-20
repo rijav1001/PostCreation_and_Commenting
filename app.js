63c6aee601799176f90348693f8b78184317e071
rijav1001 <rvu100199@gmail.com>
2022-01-20 18:17:54 +0530

    port changed

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.post('/upload', upload.single('image'), async (res, req, next) => {
     });
 });
 
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '3001';
 app.listen(port, (err) => {
     if (err) throw err;
     console.log("Server is listening on ", port);
